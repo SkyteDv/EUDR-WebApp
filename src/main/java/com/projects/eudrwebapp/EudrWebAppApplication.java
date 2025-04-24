7b33cc550fe1694ba44f880902c9f40556b9a972
Michel Hombrecher <149242970+SkyteDv@users.noreply.github.com>
2025-04-24 18:32:39 +0200

    initial commit

---
+++ src/main/java/com/projects/eudrwebapp/EudrWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.projects.eudrwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EudrWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EudrWebAppApplication.class, args);
+    }
+
+}
